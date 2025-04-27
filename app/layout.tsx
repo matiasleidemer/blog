c615758a3a2997a3949e009f9a37f641116b4f16
Matias Leidemer <matiasleidemer@gmail.com>
2025-04-27 16:55:22 -0300

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -79,6 +80,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
                 <Header />
                 <main className="mb-auto">{children}</main>
+                <SpeedInsights />
               </SearchProvider>
               <Footer />
             </div>
